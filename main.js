62adadb8eafba32dd81ad85ff1c5e53acb775058
Maria <m.delaosam@gmail.com>
2022-03-02 19:23:26 +0100

    variables js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Initializing mixer");
+
+//-- Canal 1
+load_ch1 = document.getElementById("load_ch1");
+gain_ch1 = document.getElementById("gain_ch1");
+aux1_ch1 = document.getElementById("aux1_ch1");
+aux2_ch1 = document.getElementById("aux2_ch1");
+pfl_ch1 = document.getElementById("pfl_ch1");
+mute_ch1 = document.getElementById("mute_ch1");
+main_ch1 = document.getElementById("main_ch1");
+slider_ch1 = document.getElementById("slider_ch1");
+
