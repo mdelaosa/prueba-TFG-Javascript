d98398f76bf266e3e29e609e9a142b8fba4e08e7
Maria <m.delaosam@gmail.com>
2022-03-07 16:24:41 +0100

    cambios para js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 console.log("Initializing mixer");
 
 //-- Canal 1
-load_ch1 = document.getElementById("load_ch1");
-gain_ch1 = document.getElementById("gain_ch1");
-aux1_ch1 = document.getElementById("aux1_ch1");
-aux2_ch1 = document.getElementById("aux2_ch1");
-pfl_ch1 = document.getElementById("pfl_ch1");
-mute_ch1 = document.getElementById("mute_ch1");
-main_ch1 = document.getElementById("main_ch1");
-slider_ch1 = document.getElementById("slider_ch1");
+const load_ch1 = document.getElementById('load_ch1');
+const gain_ch1 = document.getElementById('gain_ch1');
+const aux1_ch1 = document.getElementById('aux1_ch1');
+const aux2_ch1 = document.getElementById('aux2_ch1');
+const pfl_ch1 = document.getElementById('pfl_ch1');
+const mute_ch1 = document.getElementById('mute_ch1');
+const main_ch1 = document.getElementById('main_ch1');
+const slider_ch1 = document.getElementById('slider_ch1');
 
+
+function valores_slider1(){
+    let value_sliderch1 = slider_ch1.value;
+}
