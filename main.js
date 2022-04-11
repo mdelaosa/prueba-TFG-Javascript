ffd9747ade8e82e78eb17457d03ac576a0490f4b
Maria <m.delaosam@gmail.com>
2022-04-11 17:57:22 +0200

    estilo

--- main.js
+++ main.js
@@ -12,22 +12,6 @@ const main_ch1 = document.getElementById('main_ch1');
 const slider_ch1 = document.getElementById('slider_ch1');
 
 const music1 = new Audio('Guitar.mp3');
-/*music1.play();
-music1.loop =true;
-music1.playbackRate = 2;
-music1.pause();*/
-
-function valores_slider1(){
-    let value_sliderch1 = slider_ch1.value;
-}
-
-
-
-/*play_ch1.onclick = () => {
-    music1.play();
-    music1.loop = true;
-    /*music1.playbackRate = 2;
-}*/
 
 play_ch1.addEventListener("click", function (evento) {
 	// Aquí todo el código que se ejecuta cuando se da click al botón
@@ -49,4 +33,8 @@ function mute1() {
         music1.muted = false;
         document.getElementById("mute_ch1").style.backgroundColor= "#aaaaff";
     }
+}
+
+function valores_slider1(){
+    let value_sliderch1 = slider_ch1.value;
 }
