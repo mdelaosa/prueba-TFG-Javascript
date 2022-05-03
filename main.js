d14a2148ae2f71af1859a6efefac2b1faabad6f3
Maria <m.delaosam@gmail.com>
2022-05-03 17:04:19 +0200

    cambios prueba ganancia

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ console.log("Initializing mixer");
 const play_ch1 = document.getElementById('play_ch1');
 const stop_ch1 = document.getElementById('stop_ch1');
 const gain_ch1 = document.getElementById('gain_ch1');
+const value_ch1_gain = document.getElementById('value_ch1_gain');
 const aux1_ch1 = document.getElementById('aux1_ch1');
 const aux2_ch1 = document.getElementById('aux2_ch1');
 const pfl_ch1 = document.getElementById('pfl_ch1');
@@ -12,6 +13,7 @@ const main_ch1 = document.getElementById('main_ch1');
 const slider_ch1 = document.getElementById('slider_ch1');
 
 const music1 = new Audio('Guitar.mp3');
+var gain1;
 
 // CANAL 1
     // Play CH1
@@ -27,18 +29,19 @@ stop_ch1.addEventListener("click", function (evento) {
     music1.currentTime = 0;
 });
     // Gain CH1
-/*const analyserNode = new AnalyserNode(audioCtx, {
-    fftSize: 2048,
-    maxDecibels: -25,
-    minDecibels: -60,
-    smoothingTimeConstant: 0.5,
-});*/
+
+var setGain1 = audioCtx.createGain();
+source.connect(setGain1);
+setGain1.connect(audioCtx.destination);
+
+
+
     // Aux1 CH1
     // Aux2 CH1
     // PFL CH1
     // Main CH1
     // Mute CH1
-function mute1() {
+function muteCH1() {
     if (music1.muted == false) {
         music1.muted = true;
         document.getElementById("mute_ch1").style.backgroundColor= "white";
@@ -50,4 +53,49 @@ function mute1() {
 
 function valores_slider1(){
     let value_sliderch1 = slider_ch1.value;
+
+    /*
+            // jquery ui sliders to adjust track volume
+        $('.slider').slider({
+        orientation: "vertical",
+        range: "min",
+        min: 0,
+        max: 100,
+        value: 60,
+        slide: _this.track
+        });
+    */
+    /*
+        function makeMasterFader(){
+            masterGain = context.createGainNode();
+            masterGain.gain.value = 1;
+            var compressor = new tuna.Compressor({
+                               threshold: -10,    //-100 to 0
+                               makeupGain: 1,     //0 and up
+                               attack: 1,         //0 to 1000
+                               release: 0,        //0 to 3000
+                               ratio: 10,          //1 to 20
+                               knee: 5,           //0 to 40
+                               automakeup: true,  //true/false
+                               bypass: 1
+                             });
+              $('#buss').change(function(){
+              if($(this).is(':checked')){
+                compressor.bypass = 0;
+                } else {
+                  compressor.bypass = 1;}
+          
+              });
+            masterGain.connect(compressor.input);
+            compressor.connect(context.destination);
+          }
+    */
+    /*
+        const analyserNode = new AnalyserNode(audioCtx, {
+            fftSize: 2048,
+            maxDecibels: -25,
+            minDecibels: -60,
+            smoothingTimeConstant: 0.5,
+        });
+    */
 }
